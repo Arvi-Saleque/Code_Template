4f8667850788c657b94bd9447c04ab64c9ed4479
Arvi_Saleque <alifsalek.as@gmail.com>
2025-05-15 22:34:27 +0600

    Update dsu.cpp

--- code/Graph Theory/dsu.cpp
+++ code/Graph Theory/dsu.cpp
@@ -1,69 +1,29 @@
-
-struct DSU
-{
+struct DSU {
     vector<ll> par, rank, sz;
-    int nn;
-    DSU(int n) : par(n + 1), rank(n + 1, 0), sz(n + 1, 1), nn(n)
-    {
-        for (int i = 1; i <= n; ++i)
-        {
-            par[i] = i;
-        }
+    int n;
+    DSU(int n) : n(n), par(n + 1), rank(n + 1, 0), sz(n + 1, 1) {
+        iota(par.begin(), par.end(), 0);
     }
-    void make(int v)
-    {
-        par[v] = v;
-        sz[v] = 1;
+    int find(int v) {
+        return (par[v] == v ? v : (par[v] = find(par[v])));
     }
-    ll find(int v)
-    {
-        // path compression
-        if (par[v] == v)
-        {
-            return v;
-        }
-        return par[v] = find(par[v]);
+    bool same(int u, int v) {
+        return find(u) == find(v);
     }
-    ll findp(int v)
-    {
-        // no path compression
-        if (par[v] == v)
-        {
-            return v;
-        }
-        return find(par[v]);
+    int get_size(int v) {
+        return sz[find(v)];
     }
-    bool same(int u, int v)
-    {
-        return findp(u) == findp(v);
+    int count() {
+        return n; 
     }
-    int get_size(int v)
-    {
-        return sz[findp(v)];
-    }
-    int count()
-    {
-        return nn; // connected components
-    }
-    void merge(int u, int v)
-    {
-        u = findp(u);
-        v = findp(v);
-        if (u == v)
-        {
-            return;
-        }
-        nn--;
-        if (rank[u] > rank[v])
-        {
-            swap(u, v);
-        }
-        par[u] = v;
-        sz[v] += sz[u];
-        if (rank[u] == rank[v])
-        {
-            rank[v]++;
-        }
-        // v is the parent;
+    void join(int u, int v) {
+        u = find(u);
+        v = find(v);
+        n--;
+        if (rank[v] > rank[u]) swap(u, v);
+        par[v] = u;
+        sz[u] += sz[v];
+        if (rank[u] == rank[v]) rank[u]++;
+        // u is the parent;
     }
 };
