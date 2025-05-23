bacd7f1a190381804ffe107162c6e3a94792d428
Arvi_Saleque <alifsalek.as@gmail.com>
2025-05-24 03:38:46 +0600

    Update digit dp.cpp

--- code/Dynamic Programming/digit dp.cpp
+++ code/Dynamic Programming/digit dp.cpp
@@ -1,23 +1,28 @@
-string a, b;
-ll dp[11][12][2][2][2];
-ll f(int pos, int sum, bool lf, bool uf, bool lz) {
-    if(pos == a.size()) return sum | lz;
-    ll &ans = dp[pos][sum][lf][uf][lz];
+string a;
+int d;
+cin >> a >> d;
+string b((int)a.size(), '0');
+b.back() = '1';
+swap(a, b);
+function<int(int, int, bool, bool, bool)> f = [&](int pos, int rem, bool lf, bool uf, bool lz) {
+    if(pos == a.size()) {
+        if(rem == 0) return 1;
+        return 0;
+    }
+    int &ans = dp[pos][rem][lf][uf][lz];
     if(ans != -1) return ans;
     ans = 0;
     int da = a[pos] - '0', db = b[pos] - '0';
-    //cout << pos << " " << da << " " << db << "\n";
     for(int i = 0; i <= 9; i++) {
         if(i < da && !lf) continue;
         if(i > db && !uf) continue;
-
         bool lff = lf, uff = uf, lzz = lz;
         if(i > da) lff = 1;
         if(i < db) uff = 1;
         if(i != 0) lzz = 0;
-        int tmp = 0;
-        if(i == 0 && !lzz) tmp++;
-        ans += f(pos + 1, sum + tmp, lff, uff, lzz);
+        ans = (ans + f(pos + 1, (rem + i) % d, lff, uff, lzz)) % mod;
     }
     return ans;
-}
+};
+
+cout << f(0, 0, 0, 0, 1) << "\n";
