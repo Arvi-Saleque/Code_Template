cfe38f2ae9cd21aca45f7f59de785c123edec812
Arvi_Saleque <alifsalek.as@gmail.com>
2025-08-06 10:29:27 +0600

    Update bit.cpp

--- fresh code/Data structure/bit.cpp
+++ fresh code/Data structure/bit.cpp
@@ -5,7 +5,7 @@ struct BIT { //1-indexed
   BIT(int _n) {
     n = _n; t.assign(n + 1, 0);
   }
-  T query(int i) {
+  T prefixSum(int i) {
     T ans = 0;
     for (; i >= 1; i -= (i & -i)) ans += t[i];
     return ans;
@@ -13,13 +13,6 @@ struct BIT { //1-indexed
   void upd(int i, T val) {
     if (i <= 0) return;
     for (; i <= n; i += (i & -i)) t[i] += val;
-  }
-  void upd(int l, int r, T val) {
-    upd(l, val);
-    upd(r + 1, -val);
-  }
-  T query(int l, int r) {
-    return query(r) - query(l - 1);
   }
 };
 // inversion 
