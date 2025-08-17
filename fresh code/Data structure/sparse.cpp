e5cdf22c6521b4181382fec8ccf73d9f062a45f1
Arvi_Saleque <alifsalek.as@gmail.com>
2025-08-17 17:53:28 +0600

    Update sparse.cpp

--- fresh code/Data structure/sparse.cpp
+++ fresh code/Data structure/sparse.cpp
@@ -1,35 +1,34 @@
-struct Spares_table { // must be 0-based
+struct Spares_table { // now 1-based
     vector<int> a;
     vector<vector<int>> t;
     int n, q, lg;
-    Spares_table(){}
-    Spares_table(int n, int q) {
-        this->n = n;
-        this->q = q;
-        a.assign(n, 0);
-        lg = log2(n) + 1;
-        t.assign(n, vector<int>(lg));
+    Spares_table() {}
+    Spares_table(int n, int q) : n(n), q(q) {
+        lg = __lg(max(1, n)) + 1;
+        a.assign(n + 1, 0);
+        t.assign(n + 2, vector<int>(lg + 1, 0));
         take_input();
     }
     void take_input() {
-        for(auto &x : a) {
-            cin >> x;
-        }
+        for (int i = 1; i <= n; i++) cin >> a[i];
         build();
     }
     void build() {
-        for(int i = 1; i < n; i++) {
-            t[i][0] = abs(a[i] - a[i - 1]);
-        }
-        for(int j = 1; j < lg; j++) {
-            for(int i = 0; i + (1 << (j - 1)) < n; i++) {
+        if (n <= 1) return;
+        // Build on diffs d[i] = |a[i] - a[i-1]| for i = 2..n
+        for (int i = 2; i <= n; i++) t[i][0] = abs(a[i] - a[i - 1]);
+        for (int j = 1; (1 << j) <= n; j++) {
+            for (int i = 2; i + (1 << j) - 1 <= n; i++) {
                 t[i][j] = gcd(t[i][j - 1], t[i + (1 << (j - 1))][j - 1]);
             }
         }
     }
-    int query(int l, int r) { // must be 0 based index
-        int len = r - l + 1;
-        int k = log2(len);
-        return gcd(t[l][k], t[l + len - (1 << k)][k]);
+    // Query on the original array [l..r] (1-based).
+    // Returns GCD of |a[i] - a[i-1]| for i in (l..r], and 0 if l == r.
+    int query(int l, int r) {
+        if (l >= r) return 0;
+        int len = r - l;
+        int k = __lg(len);
+        return gcd(t[l + 1][k], t[r - (1 << k) + 1][k]);
     }
 };
