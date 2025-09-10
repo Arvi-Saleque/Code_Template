d33697602b299b45eeea799de5d03e73ad301ada
Arvi_Saleque <alifsalek.as@gmail.com>
2025-09-10 23:17:34 +0600

    Update my geo.cpp

--- fresh code/geometry/my geo.cpp
+++ fresh code/geometry/my geo.cpp
@@ -1,18 +1,21 @@
 struct pt {
     double x, y;
-    pt(){}
-    pt(double _x, double _y){
+    pt() {}
+    pt(double _x, double _y) {
         x = _x;
         y = _y;
     }
-    void init(double _x, double _y) {
-        x = _x; y = _y;
-    }
-    pt operator + (const pt &o) const {return pt(x + o.x, y + o.y);}
-    pt operator - (const pt &o) const {return pt(x - o.x, y - o.y);}
-    ld operator | (const pt &o) const {return x * o.x + y * o.y; }
-    ld operator * (const pt &o) const {return x * o.y - y * o.x; }
+    pt operator + (const pt &o) const { return pt(x + o.x, y + o.y); }
+    pt operator - (const pt &o) const { return pt(x - o.x, y - o.y); }
+    ld operator | (const pt &o) const { return x * o.x + y * o.y; }
+    ld operator * (const pt &o) const { return x * o.y - y * o.x; } 
     ld dis() { return hypotl(x, y); }
+    bool operator < (const pt &o) const {
+        return (x < o.x || (x == o.x && y < o.y));
+    }
+    bool operator == (const pt &o) const {
+        return (x == o.x && y == o.y);
+    }
 };
 ld pt_to_seg_dis(pt p, pt a, pt b) {
     if(((p - a) | (b - a)) < 0) {
@@ -38,4 +41,37 @@ ld dis_bet_seg(pt a, pt b, pt c, pt d) {
     ans = min(ans, pt_to_seg_dis(a, c, d));
     ans = min(ans, pt_to_seg_dis(b, c, d));
     return ans;
+}
+int orientation(pt a, pt b, pt c) {
+    ld val = (b - a) * (c - a); 
+    if (val > 0) return 1;  
+    if (val < 0) return -1;
+    return 0;
+}
+vector<pt> convex_hull(vector<pt> v) {
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    int n = v.size();
+    if (n <= 2) return v;
+    vector<pt> lower, upper;
+    for (int i = 0; i < n; i++) {
+        int j = lower.size();
+        while (j >= 2 && orientation(lower[j-2], lower.back(), v[i]) != 1) {
+            lower.pop_back();
+            j--;
+        }
+        lower.push_back(v[i]);
+    }
+    for (int i = n-1; i >= 0; i--) {
+        int j = upper.size();
+        while (j >= 2 && orientation(upper[j-2], upper.back(), v[i]) != 1) {
+            upper.pop_back();
+            j--;
+        }
+        upper.push_back(v[i]);
+    }
+    lower.pop_back();
+    upper.pop_back();
+    lower.insert(lower.end(), upper.begin(), upper.end());
+    return lower;
 }
