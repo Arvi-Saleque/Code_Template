995609e57ece33c10b8e3f189eb48ddef5bd12fb
Arvi_Saleque <alifsalek.as@gmail.com>
2025-09-14 05:56:01 +0600

    Update Hopcroft.cpp

--- fresh code/flow/Hopcroft.cpp
+++ fresh code/flow/Hopcroft.cpp
@@ -1,80 +1,69 @@
-// Hopcroft-Karp Algorithm for Maximum Bipartite Matching
-// Complexity: O(E * sqrt(V))
-// 1-indexed input nodes
-// Usage:
-//   HK hk(n);                    // Initialize with number of left side nodes
-//   hk.addEdge(u, v);           // Add edge from node u (left) to v (right)
-//   int result = hk.max_match(); // Returns size of maximum matching
+struct HopcroftKarp {
+    vector<vector<int>> g;
+    vector<int> pairU, pairV, dis;
+    int n, m;
 
-struct HK {
-  static const int inf = 1e9;
-  int n;  // Number of nodes on the left side of bipartite graph
-
-  vector<int> matchL, matchR, dist;
-  // matchL[u] = matched node of u in Right set
-  // matchR[v] = matched node of v in Left set
-  // dist[u] = distance in BFS level graph
-  vector<vector<int>> adj;  // Adjacency list for left set
-
-  // Constructor: initializes internal structures
-  HK(int n) : n(n), matchL(n + 1), matchR(n + 1), dist(n + 1), adj(n + 1) {}
-
-  // Adds an edge from node u (Left) to node v (Right)
-  void addEdge(int u, int v) {
-    adj[u].push_back(v);
-  }
-
-  // BFS to build level graph
-  // Returns true if there's an augmenting path
-  bool bfs() {
-    queue<int> q;
-    for (int u = 1; u <= n; u++) {
-      if (!matchL[u]) {
-        dist[u] = 0;  // Free node, start BFS
-        q.push(u);
-      } else {
-        dist[u] = inf;
-      }
+    HopcroftKarp(int n, int m) : n(n), m(m) {
+        g.resize(n + 1);
+        dis.resize(n + 1);
+        pairU.assign(n + 1, -1);
+        pairV.assign(m + 1, -1);
     }
-    dist[0] = inf;  // 0 represents null node
 
-    while (!q.empty()) {
-      int u = q.front(); q.pop();
-      for (auto v : adj[u]) {
-        if (dist[matchR[v]] == inf) {
-          dist[matchR[v]] = dist[u] + 1;
-          q.push(matchR[v]);
+    void add_edge(int u, int v) {
+        g[u].push_back(v); 
+    }
+
+    bool bfs() {
+        queue<int> Q;
+        for(int u = 1; u <= n; u++) {
+            if(pairU[u] == -1) {
+                dis[u] = 0;
+                Q.push(u);
+            }
+            else {
+                dis[u] = -1;
+            }
         }
-      }
-    }
-    return dist[0] != inf;  // If null node is reached, path exists
-  }
-
-  // DFS to find and augment augmenting path from node u
-  // Returns true if path is found
-  bool dfs(int u) {
-    if (!u) return true;  // Null node reached, path found
-    for (auto v : adj[u]) {
-      if (dist[matchR[v]] == dist[u] + 1 && dfs(matchR[v])) {
-        matchL[u] = v;
-        matchR[v] = u;
-        return true;
-      }
-    }
-    dist[u] = inf;  // Mark as dead end
-    return false;
-  }
-
-  // Computes and returns maximum matching
-  int max_match() {
-    int matching = 0;
-    while (bfs()) {
-      for (int u = 1; u <= n; u++) {
-        if (!matchL[u] && dfs(u)) {
-          matching++;
+        bool flag = false;
+        while(not Q.empty()) {
+            int u = Q.front(); Q.pop();
+            for(auto v : g[u]) {
+                int u2 = pairV[v];
+                if(u2 == -1) {
+                    flag = true;
+                }
+                else if(dis[u2] == -1) {
+                    dis[u2] = dis[u] + 1;
+                    Q.push(u2);
+                }
+            }
         }
-      }
+        return flag;
     }
-    return matching;
-  }
+
+    bool dfs(int u) {
+        for(auto v : g[u]) {
+            int u2 = pairV[v];
+            if(u2 == -1 || (dis[u2] == dis[u] + 1 && dfs(u2))) {
+                pairU[u] = v;
+                pairV[v] = u;
+                return true;
+            }
+        }
+        dis[u] = -1;
+        return false;
+    }
+
+    int run() {
+        int match = 0;
+        while(bfs()) {
+            for(int u = 1; u <= n; u++) {
+                if(pairU[u] == -1 && dfs(u)) {
+                    match++;
+                }
+            }
+        }
+        return match;
+    }
 };
