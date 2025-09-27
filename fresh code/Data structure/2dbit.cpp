639fb7aff808319f838322db2ae9517d28b1f9c5
Arvi_Saleque <alifsalek.as@gmail.com>
2025-09-27 07:52:58 +0600

    Update 2dbit.cpp

--- fresh code/Data structure/2dbit.cpp
+++ fresh code/Data structure/2dbit.cpp
@@ -81,3 +81,108 @@ while(q--) {
   }
 }
 }
+
+return cnt > x and prod of values > x from L....R
+  struct _2dbit {
+    struct BITadd {
+        vector<int> v;
+        int n;
+        BITadd(){}
+        BITadd(int n) : n(n) {
+            v.resize(n + 1);
+        }
+        void upd(int i, int val) {
+            for(; i <= n; i += (i & -i)) {
+                v[i] += val;
+            }
+        }
+        ll query(int i) {
+            ll ans = 0;
+            for(; i > 0; i -= (i & -i)) {
+                ans += v[i];
+            }
+            return ans;
+        }
+    };
+    struct BITmul {
+        vector<int> v;
+        int n;
+        BITmul(){}
+        BITmul(int n) : n(n) {
+            v.resize(n + 1, 1);
+        }
+        void upd(int i, int val) {
+            for(; i <= n; i += (i & -i)) {
+                v[i] = (1LL * v[i] * val) % mod;
+            }
+        }
+        ll query(int i) {
+            ll ans = 1;
+            for(; i > 0; i -= (i & -i)) {
+                ans = (1LL * ans * v[i]) % mod;
+            }
+            return ans;
+        }
+    };
+    vector<vector<int>> cords;
+    vector<BITadd> cnt;
+    vector<BITmul> prod;
+    int n;
+    _2dbit(int n) : n(n) {
+        cords.resize(n + 1);
+        cnt.resize(n + 1);
+        prod.resize(n + 1);
+    }
+    void add(int i, int val) {
+        for(; i <= n; i += (i & -i)) {
+            cords[i].push_back(val);
+        }
+    }
+    void build() {
+        for(int i = 1; i <= n; i++) {
+            auto &v = cords[i];
+            sort(v.begin(), v.end());
+            v.erase(unique(v.begin(), v.end()), v.end());
+            cnt[i] = BITadd(v.size());
+            prod[i] = BITmul(v.size());
+        }
+    }
+    int getidx(int p, int x) {
+        auto &v = cords[p];
+        return (lower_bound(v.begin(), v.end(), x) - v.begin() + 1);
+    }
+    void upd(int i, int val, int delta) {
+        for(; i <= n; i += (i & -i)) {
+            int id = getidx(i, val);
+            cnt[i].upd(id, delta);
+            if(delta == 1) prod[i].upd(id, val % mod);
+            else prod[i].upd(id, invmod(val));
+        }
+    }
+    array<ll, 2> query(int i, int x) {
+        array<ll, 2> ans = {0, 1};
+        for(; i > 0; i -= (i & -i)) {
+            int id = getidx(i, x);
+            int sz = cords[i].size();
+            if(id <= sz) {
+                ll tot = prod[i].query(sz);
+                ll less = prod[i].query(id - 1);
+                ll val = (tot * invmod(less)) % mod;
+                int ctot = cnt[i].query(sz);
+                int cless = cnt[i].query(id - 1);
+                int cval = ctot - cless;
+                ans[0] += cval;
+                ans[1] = (ans[1] * val) % mod;
+            }
+        }
+        return ans;
+    }
+    array<ll, 2> rangeQ(int l, int r, int x) {
+        auto tot = query(r, x);
+        auto less = query(l - 1, x);
+        array<ll, 2> res = {0, 1};
+        res[0] = (tot[0] - less[0]);
+        res[1] = (tot[1] * invmod(less[1])) % mod;
+        return res;
+    }
+};
